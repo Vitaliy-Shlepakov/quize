10d07c053d8523de936fce92579939c918de9f49
Vik <shlepakov58@gmail.com>
2020-03-05 10:13:53 +0300

    Updates

---
+++ precache-manifest.6a207b1e501be949a3bee04380ece4e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3d617e8a1046bb57fb7b8deee90d2e9",
+    "url": "/artem_test/index.html"
+  },
+  {
+    "revision": "57c9392b953917dc644f",
+    "url": "/artem_test/static/css/main.ac166b78.chunk.css"
+  },
+  {
+    "revision": "0b61c92e530c9042cbee",
+    "url": "/artem_test/static/js/2.2597c243.chunk.js"
+  },
+  {
+    "revision": "0bc5220d92bfe34c3dec2f124a7bb286",
+    "url": "/artem_test/static/js/2.2597c243.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57c9392b953917dc644f",
+    "url": "/artem_test/static/js/main.da3d0fba.chunk.js"
+  },
+  {
+    "revision": "c23e2c58e1398f194788",
+    "url": "/artem_test/static/js/runtime-main.e11e847e.js"
+  }
+]);
